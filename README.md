4abc597aaf27628625fe2ea186c71b3cd003a10f
Ryan Brandt <rnb52@scarletmail.rutgers.edu>
2019-06-24 21:17:55 -0400

    oof

--- README.md
+++ README.md
@@ -1,25 +1,19 @@
-Pretty simple linear regression calculator I made during a really slow day at a summer job.
+# Regression Calculator
+
+A very simple linear regression calculator I made quickly to learn a bit about making desktop GUIs with Python.
 
 Really limited as far as its scope of actual applications, but pretty smooth for an introductory statistics or linear algebra course.
 
-Uses pandas and numpy for the data handling side (pandas for dataframes/series, numpy for linear algebra).
-
-Uses tkinter for a really bare-bones GUI; users can quit (exit) the application or submit their data with, obviously, submit.
-
 <img width="595" alt="screen shot 2018-09-07 at 10 21 10 pm" src="https://user-images.githubusercontent.com/37029617/45249474-e5abc400-b2ee-11e8-96e1-d0f1c7041bca.png">
 
-Unfortunately, right now, the calculator only supports complete data and wont make the computation if the number of x-values =/= the number of y-values. Ill probably add support for incomplete data eventually.
-
-<img width="796" alt="screen shot 2018-09-07 at 10 33 04 pm" src="https://user-images.githubusercontent.com/37029617/45249476-f2c8b300-b2ee-11e8-88b5-b29f824c546e.png">
-
-Upon making a computation, the window prompts users to press the input new data button if they want to compute a new regression line. If the user just enters new data without pressing input new data, the computation will still be correct, but other things can potentially get a little wacky, still have to work on that. The input new data button simply restarts the application. 
-
-Also, user's are now surprised with a graph me button, which allows the user to choose to view a plot of their regression line and input data.
+Performs the numeric computation and then offers users the option to generate a corresponding plot
 
 <img width="697" alt="screen shot 2018-09-07 at 10 34 34 pm" src="https://user-images.githubusercontent.com/37029617/45249479-04aa5600-b2ef-11e8-9288-cc4aafa8e8d2.png">
 
-If the user presses graph me, a plot is generated via matplotlib's pyplot, with the data as a scatter plot against the regression line generated in the previous step.
-
 <img width="1276" alt="screen shot 2018-09-07 at 10 35 07 pm" src="https://user-images.githubusercontent.com/37029617/45249482-11c74500-b2ef-11e8-9fab-c98fa8849830.png">
 
-Overall, still a work in progress. In the future ill address the issues mentioned above and hopefully add support for multiple regression and (maybe?) other regression methods, curvature (i.e. parabolic fitting), a prediction option and, of course, a nicer GUI.
+### Implementation
+1) Pandas: data structures
+2) Numpy: linear algebra
+3) Matplotlib: visualization
+4) Tkinter: GUI
--- regressionCalc.py
+++ regressionCalc.py
@@ -11,52 +11,53 @@ from functools import partial
 
 def leastSquares( ):
     
- 
-    
-
-    def myLine(x,x_vec):                                                        #get regression regression line
+    # get regression line
+    def myLine(x,x_vec):                                                       
         a = x_vec[1,0]
         return x_vec[0,0] + (x*a)
     
+    # graph line against data
     def graphMe(x,y, x_vec):
-        y_reg = y.astype(float)                                                 #graph line against data
-        for i in range(len(y)):                                                 #get regression y-values
+        y_reg = y.astype(float) 
+        # gaph y-vals                                                
+        for i in range(len(y)):                                                 
             y_reg[i] = myLine(x[i], x_vec)
-            
-        plt.figure("Regression Plot")                                           #plot
+        
+        # generate plot    
+        plt.figure("Regression Plot")                                   
         plt.scatter(x, y, color = "blue")
         plt.plot(x, y_reg, color = "red")
         plt.xlabel('x-axis')
         plt.ylabel('y-axis')    
         plt.title("Regression line (red) vs Data (blue)")
         
-        plt.show()                                                              #/plot
+        plt.show()                                                             
     
-   
-    def restartRegression():                                                    #restart application; if input new data pushed
+   	# hard reset app w system call
+    def restartRegression():                                                    
         python = sys.executable
         os.execl(python, python, * sys.argv)
     
-    def printMe(myLine):                                                        #out computed regression line to UI window in str
+    # out computed line to UI
+    def printMe(myLine):                                                        
+        master.geometry("700x400")                                         
+        Label(master, text = "Regression Line:", background = "bisque").grid(column = 0, row = 4)
+        Label(master, text = myLine, background = "bisque").grid(column = 0, row = 5)
+        Label(master, text = "Press input new data for a new regression line", background = "yellow").grid(column = 0, row = 6)
             
-            master.geometry("700x400")                                          #adjust GUI window size for text
-            Label(master, text = "Regression Line:", background = "bisque").grid(column = 0, row = 4)
-            Label(master, text = myLine, background = "bisque").grid(column = 0, row = 5)
-            Label(master, text = "Press input new data for a new regression line", background = "yellow").grid(column = 0, row = 6)
-            
-    def computations(df,y):             
-        
-        
-        if len(df) != len(y):                                                   #catch user error, only takes equal num x,y
-            master.geometry("800x400")                                          #adjust GUI window to fit text                                                                        
+    # main computational method
+    def computations(df,y):   
+    	# need equal x and y          
+        if len(df) != len(y):                                                   
+            master.geometry("800x400")                                                                                                           
             L =  Label(master, text = "Need equal number of x and y-values, press input new data and restart", background = "red")
             L.grid(column = 0, row = 4)
             
             raise ValueError("User must input equal amount of x and y-values")
         
         
-         
-        ones = pd.Series([1]*len(df))                                                   #linear algebra 
+        # Simple least squares w/ linear algebra
+        ones = pd.Series([1]*len(df))                                                   
         df_fix = pd.concat([ones, df], axis = 1)
         
         df_fix = df_fix.astype(int)
@@ -65,7 +66,6 @@ def leastSquares( ):
                                                         
         df_trans = pd.DataFrame.transpose(df_fix)
         
-    
         df_t_df = np.matmul(df_trans, df_fix)
                                                             
         df_t_y = np.matmul(df_trans, y)
@@ -79,58 +79,48 @@ def leastSquares( ):
         df_t_df[0,0] = df_t_df_d
         df_t_df[0,df_t_df.shape[0]-1] = -df_t_df[0,df_t_df.shape[0]-1]
         df_t_df[df_t_df.shape[1] - 1, 0] = -df_t_df[df_t_df.shape[1]-1, 0]
-    
-    
-    
-        x_vec = (1/my_det)*np.matmul(df_t_df, df_t_y)                                   #/linear algebra
-              
-   
+ 
+ 		# solution vector   
+        x_vec = (1/my_det)*np.matmul(df_t_df, df_t_y)                                 
+        # get line, call print utility      
         myLine = "y = " + str(x_vec[0,0]) + " + x" + str(x_vec[1,0])
         printMe(myLine)
             
         command_with_args = partial(graphMe,df, y, x_vec)
         Button(master, text = "Graph Me", command = command_with_args , highlightbackground = "bisque").grid(row = 7, column = 0)
      
-       
-   
-    def getInt( ):                                                                       #throw GUI input to computations
+    # grab user input and pass to perform computations
+    def getInt( ):                                                                       
         x_vals = pd.Series(e1.get( ).split(","))
         y_vals = pd.Series(e2.get( ).split(","))
         
         computations(x_vals, y_vals)
         
-                                                
-            
-    
-    master = Tk()                                                                                                      #GUI
+    # generate GUI
+    master = Tk()                                                                                                      
     master.title("Regression Calculator")
     master.geometry("600x400")
     master.configure(background = "bisque")
     
+    # label elements
     Label(master, text = "Enter values as comma-seperated", background = "bisque").grid(row = 0)
     Label(master, text = "x-values:", background = "bisque").grid(row = 1)
     Label(master, text = "y-values:", background = "bisque").grid(row = 2)
     
-    
+    # input elements
     e1 = Entry(master, background = "bisque", highlightthickness = 0)
     e2 = Entry(master, background = "bisque", highlightthickness = 0)
-    
     e1.grid(row = 1, column = 1)
     e2.grid(row = 2, column = 1)
     
+    # button elements
     Button(master, text = "Quit", command = master.quit, highlightbackground = "bisque").grid(row = 3, column = 0, sticky = W, pady = 4)
     Button(master, text = "Submit", command = getInt, highlightbackground = "bisque").grid(row = 3, column = 1, sticky = W, pady = 4)
     Button(master, text = "Input New Data", command = restartRegression, highlightbackground = "bisque").grid(row = 3, column = 2, sticky = W, pady = 4)
    
-    
     master.call('wm', 'attributes', '.', '-topmost', '1')
     
-   
-    
-    mainloop( )                                                                                                           #/GUI
-   
-    
-
-   
-        
-leastSquares()                                                                          #run app                                                                         #run app
+    mainloop( )                                                                                                           
+       
+# run app
+leastSquares( )                                                                                                                                                  #run app
